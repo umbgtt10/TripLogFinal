3ffd4cc99e5bdd02d279e168c8987adbc095d582
Umberto <Umbertogotti1979@gmail.com>
2019-02-11 06:25:05 +0100

    Testing of the persistency layer finalized.

--- TripLog.Server/DbreezeTripLogPersistency.cs
+++ TripLog.Server/DbreezeTripLogPersistency.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using DBreeze;
+using Newtonsoft.Json;
 using TripLog.Models;
 
 namespace TripLog.Server
 {
     public class DbreezeTripLogPersistency : TripLogPersistency
     {
-        private DBreezeEngine _db;
-        protected DirectoryInfo _dbDirectory;
+        protected string TableName = "TripLogTable";
+        protected DBreezeEngine Db;
+        private readonly DirectoryInfo _dbDirectory;
 
         public DbreezeTripLogPersistency(DirectoryInfo directory)
         {
             _dbDirectory = directory;
-            _db = new DBreezeEngine(directory.FullName);
+            Db = new DBreezeEngine(directory.FullName);
         }
 
         public void Setup()
@@ -26,19 +29,56 @@ namespace TripLog.Server
 
         public void Dispose()
         {
-            if (_db != null)
+            if (Db != null)
             {
-                _db.Dispose();
+                Db.Dispose();
             }
         }
 
         public void Add(TripLogEntry value)
         {
+            using (var transaction = Db.GetTransaction())
+            {
+                transaction.Insert(TableName, value.Id, Serialize(value));
+                transaction.Commit();
+            }
         }
 
         public IEnumerable<TripLogEntry> GetAll()
         {
-            return new TripLogEntry[] { new TripLogEntry(), new TripLogEntry() };
+            IList<TripLogEntry> result;
+
+            using (var transaction = Db.GetTransaction())
+            {
+                var selection = transaction.SelectForward<string, string>(TableName);
+                result = selection.Select(elem => Deserialize(elem.Value)).ToList();
+                transaction.Commit();
+            }
+
+            return result;
+        }
+
+        private string Serialize(TripLogEntry entry)
+        {
+            return JsonConvert.SerializeObject(entry);
+        }
+
+        private TripLogEntry Deserialize(string serializedTripLogEntry)
+        {
+            var entry = JsonConvert.DeserializeObject<TripLogEntry>(serializedTripLogEntry);
+
+            TripLogEntry result = new TripLogEntry
+            {
+                Id = entry.Id,
+                Title = entry.Title,
+                Latitude = entry.Latitude,
+                Longitude = entry.Longitude,
+                Date = entry.Date,
+                Rating = entry.Rating,
+                Notes = entry.Notes
+            };
+
+            return result;
         }
     }
 }
--- TripLog/TripLog/Models/TripLogEntry.cs
+++ TripLog/TripLog/Models/TripLogEntry.cs
@@ -4,11 +4,24 @@ namespace TripLog.Models
 {
     public class TripLogEntry
     {
+        public string Id { get; set; }
         public string Title { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public DateTime Date { get; set; }
         public int Rating { get; set; }
         public string Notes { get; set; }
+
+        public TripLogEntry()
+        {
+            Title = string.Empty;
+            Id = Guid.NewGuid().ToString();
+            Notes = string.Empty;
+        }
+
+        public TripLogEntry(string title) : this()
+        {
+            Title = title;
+        }
     }
 }
